bac85e5cdd0770e30e115f9115ed2657091fdc5f
DAO <chimii@126.com>
2025-04-03 10:28:25 +0800

    v1.8

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,13 +21,13 @@ export const metadata = {
     generator: 'v0.dev'
 }
 
-export default function RootLayout() {
+export default function RootLayout({ children }) {
   return (
     <html lang="zh-CN" suppressHydrationWarning>
       <head />
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          
+          {children}
         </ThemeProvider>
       </body>
     </html>
